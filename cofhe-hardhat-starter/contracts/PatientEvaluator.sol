10be2caa94ffa85aef7ae5f1b2b55204d5ddde9a
Coleman Irby <ckirbyee92@gmail.com>
2026-03-07 22:47:24 -0600

    update contract

--- cofhe-hardhat-starter/contracts/PatientEvaluator.sol
+++ cofhe-hardhat-starter/contracts/PatientEvaluator.sol
@@ -9,6 +9,7 @@ contract PatientEvaluator {
     euint32 public ZERO;
     // ["A+": 0, "A-": 1, "B+": 2, "B-": 3, "AB+": 4, "AB-": 5, "O+": 6, "O-": 7]
     euint32[8] public countArray; 
+    uint32[8] public decryptedCountArray;
     ebool public isInitialized;
 
     constructor() {
@@ -16,8 +17,10 @@ contract PatientEvaluator {
         ZERO = FHE.asEuint32(0);
         count = FHE.asEuint32(0);
 
+
         for (uint i = 0; i < 8; i++) {
             countArray[i] = ZERO;
+            decryptedCountArray[i] = 0;
         }
 
         isInitialized = FHE.asEbool(false);
@@ -50,14 +53,15 @@ function countMatch(InEuint32[] calldata patientTypes) public {
 }
 
 function countMatchSpecific(InEuint32[] calldata patientTypes, InEuint32 calldata bloodType) public {
+    euint32 bloodTypeEuint = FHE.asEuint32(bloodType);
     for (uint i = 0; i < patientTypes.length; i++) {
         // 0, 1, 2,... 7
         euint32 patientType = FHE.asEuint32(patientTypes[i]);
-        euint32 bloodTypeEuint = FHE.asEuint32(bloodType);
         ebool isMatch = FHE.eq(patientType, bloodTypeEuint);
         euint32 increment = FHE.select(isMatch, ONE, ZERO);
         count = FHE.add(count, increment);
     }
+    FHE.allowSender(count);
     FHE.allowThis(count);
 }
 
@@ -65,6 +69,7 @@ function reset() public {
     count = ZERO;
     for (uint i = 0; i < 8; i++) {
         countArray[i] = ZERO;
+        decryptedCountArray[i] = 0;
     }
 
     FHE.allowThis(count);
@@ -73,6 +78,25 @@ function reset() public {
 
 function getAllTypesCount() public view returns (euint32[8] memory) {
     return countArray;
+}
+
+function decryptCountArray() public {
+    for (uint i = 0; i < 8; i++) {
+        FHE.decrypt(countArray[i]);
+    }
+}
+
+function retrieveDecryptedValues() public returns(uint32[8] memory decryptedVals , bool allReady) {
+
+    allReady = true;
+    for (uint i = 0; i < 8; i++) {
+        (uint32 val1, bool ready1) = FHE.getDecryptResultSafe(countArray[i]);
+        allReady = allReady && ready1;
+        decryptedCountArray[i] = val1;
+    }
+
+    return (decryptedCountArray, allReady);
+
 }
 
 function getMatchedTypeCount() public view returns (euint32 finalcount) {
